91d57b1670c81f9168671587781f6a079ed9fd39
Başak Sökmen <basaksokmeen@gmail.com>
2022-03-10 11:09:39 +0300

    Proje dosyası ekle.

---
+++ UsAlma/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UsAlma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double top = 1, taban, us;
+            Console.WriteLine("Taban giriniz = ");
+            taban = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("üs giriniz = ");
+            us = Convert.ToDouble(Console.ReadLine());
+
+            for (int i = 1; i <= us; i++)
+            {
+                top = top * taban;
+            }
+            Console.WriteLine(taban + " Üssü " + us + " = {0} ", top);
+
+            Console.ReadKey();
+        }
+    }
+}
